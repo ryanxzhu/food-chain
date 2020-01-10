35dda6c3e8a037dbdc610fa4dde5533ad0d92ca2
Ryan Zhu <ryanxzhu@gmail.com>
2020-01-10 11:47:21 -0500

    refactored the code to be separated into functions. Added the animate function

--- main.js
+++ main.js
@@ -1,17 +1,135 @@
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 const controlsSection = document.querySelector('#controls');
 const sunlightSlider = document.querySelector('#sunlightSlider');
 const worldSpeedSlider = document.querySelector('#worldSpeedSlider');
 const h4Container = document.querySelector('#h4-container');
 const h4 = document.querySelector('h4');
-controlsSection.style.height = '95px';
-canvas.style.height = window.innerHeight * 0.99 + 'px';
 const gameSpeedArray = [ 1, 2, 3, 4 ];
 var gameSpeed = gameSpeedArray[0];
+var mouse = {
+	x: -200,
+	y: 200
+};
 
 init();
 
+animate();
+
+// initilisation function
 function init() {
+	controlsSection.style.height = '95px';
+
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+
+	const brownColorArray = createBrownArray();
+
+	canvas.style.backgroundColor = brownColorArray[50];
+
+	// Event Listeners for sunlight and work speed range input
+	sunlightSlider.addEventListener('input', function() {
+		canvas.style.backgroundColor = brownColorArray[this.value];
+	});
+
+	h4Container.style.left = '0px';
+	worldSpeedSlider.addEventListener('input', function() {
+		h4Container.style.left = parseFloat(this.value) * 93.5 + 'px';
+		gameSpeed = gameSpeedArray[this.value];
+		h4.innerHTML = gameSpeed + 'x' + '<span></span>';
+	});
+
+	// Basic Event Listeners
+	addEventListener('mousemove', function(e) {
+		mouse.x = e.x;
+		mouse.y = e.y;
+
+		// Hover down menu due to mouse moved to the top of the screen
+		if (mouse.y < 100) {
+			controlsSection.style.transform = 'translateY(100px)';
+		} else {
+			controlsSection.style.transform = 'translateY(0px)';
+		}
+	});
+
+	addEventListener('resize', function() {
+		canvas.width = innerWidth;
+		canvas.height = innerHeight;
+	});
+}
+
+//Utility Functions
+
+function randomIntFromRange(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function resultantVelocity(xVelocity, yVelocity) {
+	return Math.sqrt(Math.pow(xVelocity, 2) + Math.pow(yVelocity, 2));
+}
+
+function angleOfDirection(xVelocity, yVelocity, resultantVelocity) {
+	if (xVelocity >= 0 && yVelocity >= 0) {
+		return Math.asin(xVelocity / resultantVelocity);
+	}
+	if (xVelocity >= 0 && yVelocity <= 0) {
+		return Math.PI / 2 + Math.acos(xVelocity / resultantVelocity);
+	}
+	if (xVelocity <= 0 && yVelocity <= 0) {
+		return Math.PI + Math.asin(Math.abs(xVelocity) / resultantVelocity);
+	}
+	if (xVelocity <= 0 && yVelocity >= 0) {
+		return Math.PI * 3 / 2 + Math.asin(yVelocity / resultantVelocity);
+	}
+}
+
+function changeAngle(angle) {
+	let random = Math.random();
+	let newAngle = random < 0.5 ? angle + 0.1 : angle - 0.1;
+	newAngle = newAngle < 0 ? Math.PI * 2 + newAngle : newAngle;
+	newAngle = newAngle >= Math.PI * 2 ? newAngle - Math.PI * 2 : newAngle;
+
+	return newAngle;
+}
+
+function findXVelocity(angle, velocity) {
+	if (angle <= Math.PI / 2) {
+		return Math.sin(angle) * velocity;
+	}
+	if (angle <= Math.PI) {
+		angle = angle - Math.PI / 2;
+		return Math.cos(angle) * velocity;
+	}
+	if (angle <= Math.PI * 3 / 2) {
+		angle = angle - Math.PI;
+		return Math.sin(angle) * velocity * -1;
+	}
+	if (angle <= Math.PI * 2) {
+		angle = angle - Math.PI * 3 / 2;
+		return Math.cos(angle) * velocity * -1;
+	}
+}
+
+function findYVelocity(angle, velocity) {
+	if (angle <= Math.PI / 2) {
+		return Math.cos(angle) * velocity;
+	}
+	if (angle <= Math.PI) {
+		angle = angle - Math.PI / 2;
+		return Math.sin(angle) * velocity * -1;
+	}
+	if (angle <= Math.PI * 3 / 2) {
+		angle = angle - Math.PI;
+		return Math.cos(angle) * velocity * -1;
+	}
+	if (angle <= Math.PI * 2) {
+		angle = angle - Math.PI * 3 / 2;
+		return Math.sin(angle) * velocity;
+	}
+}
+
+// creates an array of shades of brown. Used for sunlight range slider
+function createBrownArray() {
 	var rgb = {
 		r: 0,
 		g: 0,
@@ -53,24 +171,21 @@ function init() {
 			'rgb(' + brownColorArray[i].r + ', ' + brownColorArray[i].g + ', ' + brownColorArray[i].b + ')';
 	}
 
-	canvas.style.backgroundColor = brownColorArray[50];
+	return brownColorArray;
+}
 
-	sunlightSlider.addEventListener('input', function() {
-		canvas.style.backgroundColor = brownColorArray[this.value];
-	});
+//Animation loop
+function animate() {
+	requestAnimationFrame(animate);
 
-	h4Container.style.left = '0px';
-	worldSpeedSlider.addEventListener('input', function() {
-		h4Container.style.left = parseFloat(this.value) * 93.5 + 'px';
-		gameSpeed = gameSpeedArray[this.value];
-		h4.innerHTML = gameSpeed + 'x' + '<span></span>';
-	});
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	c.fillText('mouse', mouse.x, mouse.y);
+}
 
-	document.body.addEventListener('mousemove', function(e) {
-		if (e.y < 100) {
-			controlsSection.style.top = '0px';
-		} else {
-			controlsSection.style.top = '-100px';
-		}
-	});
+// Objects
+function Organism(x, y, size, color) {
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	this.color = color;
 }
